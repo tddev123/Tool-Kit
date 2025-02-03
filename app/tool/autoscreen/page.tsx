1c2b899979d47ff88daf29354f788720a5132b61
Tdev <tyler26daniel@gmail.com>
2025-02-02 22:56:56 -0800

    2nd big change

--- app/tool/autoscreen/page.tsx
+++ app/tool/autoscreen/page.tsx
@@ -1,9 +1,24 @@
+import ParticlesComponent from "@/components/Particles";
 import Screenshotter from "@/components/screenshotter";
 
 export default async function autoscreenshotter() {
   return (
     <>
-  <Screenshotter/>
+          {/* Particles component behind everything */}
+          <ParticlesComponent id="particles" className="absolute inset-0 -z-100" />,
+          
+
+    {/* Content above particles */}
+        <div className="relative z-10">
+        <Screenshotter/>
+       
+          <main className="flex-1 flex flex-col gap-6 px-4">
+           
+          </main>
+        </div>
+
+  
+  
     </>
   );
 }
